c1037a58a7b425488f6341c977f004d9690a22ce
Tserenjigmed Shagdar <jigmee2006@gmail.com>
2022-03-27 19:52:48 +0800

    changed style

---
+++ front.js
@@ -0,0 +1,26 @@
+function getData() {
+      fetch("http://localhost:3000/games", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                  result.map(x => {
+                        const main = document.getElementById('main')
+                        main.innerHTML += `
+                        <div class="games" style="background-color: ${x.color}"> 
+                        <h1 class="title${x.count}">${x.short}</h1>
+                        <img src="${x.img}" alt="247" style="width:483px;height:100px;">
+                        <p class="title${x.count} align">Likes: ${x.like}</p>
+                        <a class="title" target="_blank" href=${x.link}>link</a>
+                        <button class="button${x.count-1}">Like</button>
+                        </div>`
+                        })
+      //                   <h1 class="title3 ">2. Addicting Games. Best For: Single-player games.</h1>
+      //   <img src="addicting games.png" alt="AG" style="width:600px;height:100px;"> <h1 class="title3 ">(Link: https://www.addictinggames.com/)</h1>
+      //   <h1 class="title4 ">3. Agame. Best For: Games on multiple devices, single and multi-player online games </h1>
+      //   <img src="agame.png" alt="agame"style="width:600px;height:100px;"> <h1 class="title4 ">(Link: https://www.agame.com/)</h1>
+            })
+            .catch(error => console.log('error', error));
+}
+var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+};
--- nodejs/doko.js
+++ nodejs/doko.js
@@ -16,11 +16,30 @@ var games = [
     {
         short: '1. 247 Games. Best For: Classic games, card games and puzzles.',
         img: '247game.png',
-        link: '',
+        link: 'https://www.247games.com/',
+        like: '0',
+        color: '#F6DF42',
+        count:2,
+    },{
+        short: '2. Addicting Games. Best For: Single-player games.',
+        img: 'addicting games.png',
+        link: 'https://www.addictinggames.com/',
         like: 0,
-        color: '#F6DF42'
+        color: '#171717',
+        count: 3,
+    },{
+        short: '3. Agame. Best For: Games on multiple devices, single and multi-player online games',
+        img: 'agame.png',
+        link: 'https://www.agame.com/',
+        like: 0,
+        color: '#0364A0',
+        count: 4,
     }
 ]
+{/* <h1 class="title3 ">2. Addicting Games. Best For: Single-player games.</h1>
+        <img src="addicting games.png" alt="AG" style="width:600px;height:100px;"> <h1 class="title3 ">(Link: https://www.addictinggames.com/)</h1>
+        <h1 class="title4 ">3. Agame. Best For: Games on multiple devices, single and multi-player online games </h1>
+        <img src="agame.png" alt="agame"style="width:600px;height:100px;"> <h1 class="title4 ">(Link: https://www.agame.com/)</h1> */}
 
 app.get('/', function (req, res) {
     res.send('Hello World')
@@ -48,6 +67,7 @@ app.put('/games', function (req, res) {
     })
     res.send('Success')
 })
+
 
 app.listen(3000);
 
